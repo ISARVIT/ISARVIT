d58749cdea3d4d9188fcf71a36366284c18bf71c
Joel Kalil <53882286+joelkalil@users.noreply.github.com>
2021-11-23 13:32:13 +0100

    Add files via upload

--- backend/flaskr/static/MySQL/make.sql
+++ backend/flaskr/static/MySQL/make.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE `mydb` IF NOT EXISTS;
 
 CREATE TABLE `users` IF NOT EXISTS(
-    ID INT NOT NULL,
     FirstName VARCHAR(255),
     LastName VARCHAR(255),
     Username VARCHAR(255),
     Password VARCHAR(255),
     
-    PRIMARY KEY(ID)
+    PRIMARY KEY(Username)
 );
 
