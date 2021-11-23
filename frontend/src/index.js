176f500cfec8688d17d23dea690d77b0c15a8674
Andreis Purim <AndreisPurim@gmail.com>
2021-11-23 11:39:03 +0100

    New changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';
+
+import './Source/index.css';
+import LandingPage from './Pages/LandingPage';
+import Login from './Pages/Login';
+import UserHome from './Pages/UserHome';
+import Forms from './Pages/Forms';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <HashRouter>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/user' element={<UserHome />} />
+      <Route path='/forms' element={<Forms />} />
+    </Routes>
+  </HashRouter>
+  ,document.getElementById('root'));
