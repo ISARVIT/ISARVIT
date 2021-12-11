1058535afc1c54041e298f7998958933214398e1
Andreis Purim <AndreisPurim@gmail.com>
2021-12-11 15:04:15 +0100

    Started using Jodit

--- frontend/src/Pages/Creator/Questions.js
+++ frontend/src/Pages/Creator/Questions.js
@@ -1,33 +1,48 @@
 import React from 'react';
 
+import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
-import Questions from './Questions.js';
-
-var parse = require('html-react-parser');
-
+import JoditEditor from "jodit-react";
 
 
 export default function Text(props){
-    const [t,setT] = React.useState('');
-    const handle = (event) => {
-        var text=event.target.value;
+	const editor = React.useRef(null)
+	const [content, setContent] = React.useState('')
 
-        if(text.indexOf("My name")>-1){
-            text=text.replace("My name", "<span class='blue'>My name</span>");
-        }
-    
-        if(text.indexOf("my age")>-1){
-            text=text.replace("my age", "<span class='blue'>my age</span>");
-        }
-    
-        document.getElementById("colorText").innerHTML=text;
-    }
+	const config = {
+        readonly: false,
+        extraButtons: [
+            {
+                name: 'Var',
+                iconURL: 'http://xdsoft.net/jodit/logo.png',
+                list: {
+                    double:'Variable 1',
+                    single:'Variable 2',
+                    none:'Variable 3'
+                },
+                exec: function(editor, t, {control}) {
+                    console.log(control.args);
+                    editor.s.insertHTML('<span>['+control.args+']</span>');
+                },
+                template: (editor, key, value) => {
+                    return '<span>'+value+'</span>';
+                }
+            }
+        ],
+      }
+
     return (
-        <>
-            <textarea onKeyUp={handle}></textarea>
-            <div id="colorText"></div>
-        </>
+        <Grid item style={{width:'80vw',height:'80vh'}}>
+            <JoditEditor
+            	ref={editor}
+                value={content}
+                config={config}
+		        tabIndex={1} // tabIndex of textarea
+		        onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                onChange={newContent => {}}
+            />
+        </Grid>
     )
 }
