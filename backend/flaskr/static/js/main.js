2401b105b3a2bef65fb0f497c9b65b0fdef55b73
Joel Kalil <53882286+joelkalil@users.noreply.github.com>
2021-12-27 00:44:06 +0100

    Add files via upload

--- backend/flaskr/static/js/main.js
+++ backend/flaskr/static/js/main.js
@@ -1,5 +1,3 @@
-
-
 function back(){
     var path = document.getElementById("Login");
     path.innerHTML = `<input type = "submit" value = "Sign In" onclick="login();"/>
@@ -33,7 +31,6 @@ function getForms(){
     var User = document.getElementById("nameUser");
     User = User.textContent;
     User = User.substring(8,);
-
     var URL = "API/getForms/" + User;
 
     $.ajax({
@@ -43,14 +40,81 @@ function getForms(){
         dataType: 'json' ,
     }).done(function(data) {
         var path = document.getElementById("Forms");
+        data = eval(data)
         var size = data['length'];
-        console.log(size);
-        /*for(var i=0; i < size; i++){
-            path.innerHTML += `
-                <p> ${data[i]} </p>
-            `;
-        }*/
+        var code = ''
+
+        code += `
+        <table border="1">
+            <tr>
+                <td> Formulary Name </td>
+                <td> Creator </td>
+                <td> Content </td>
+                <td> Formulary ID </td>
+            </tr>`;
+
+        for(var i=0; i < size; i++){
+            code += `
+            <tr>
+                <td> ${data[i]['FormName']} </td>
+                <td> ${data[i]['Creator']} </td>
+                <td> ${data[i]['Content']} </td>
+                <td> ${data[i]['FormID']} </td>
+            </tr>`;
+        }
         
-        console.log(data);
+        code += `</table>`;
+
+        code += `<br><input type = "submit" value = "Hide Forms" onclick="hideForms();"/>
+        <input type = "submit" value = "Add Forms" onclick="addForms();"/>`
+        path.innerHTML = code;
+    });
+}
+
+function hideForms(){
+    var path = document.getElementById("Forms");
+    var code = `<input type = "submit" value = "Show Forms" onclick="getForms();"/>
+    <input type = "submit" value = "Add Forms" onclick="addForms();"/>`;
+    path.innerHTML = code;
+}
+
+
+function addForms(){
+    var path = document.getElementById("Forms");
+    var code = '';
+
+    code += `
+    <form onSubmit = "sendForm();">
+        <p> Form Name : <input type = "text" name = "formName" /></p>
+        <p> Content : <input type = "text" name = "content" /></p>
+        <p><input type = "submit" value = "Submit" /></p>
+    </form> `;
+
+    path.innerHTML = code;
+}
+
+function sendForm(){
+
+    var User = document.getElementById("nameUser");
+    User = User.textContent;
+    User = User.substring(8,);
+
+    var formName = $('input[name=formName]').val();
+    var content = $('input[name=content').val();
+
+    $.ajax({
+        type: "GET",
+        url: 'API/addForm',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json' ,
+        data: {
+            'formName': formName,
+            'creator': User,
+            'content': content,
+        },
+    }).done(function(data) {
+        data = eval(data);
+        alert(data.status);
+        
     });
 }
